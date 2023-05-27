64d776378b5d958366a010bdb4c67e94ba88e36e
onemore <909413016@qq.com>
2023-05-28 00:00:18 +0800

    不再安装到全局目录中了

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,15 +1,12 @@
 :: https://stackoverflow.com/questions/17063947/get-current-batchfile-directory
-@echo off
-:: --HAS ENDING BACKSLASH
+@REM @echo off
 set batdir=%~dp0
-:: --MISSING ENDING BACKSLASH
-:: set batdir=%CD%
 cd /d "%batdir%"
 
 :: 判断文件夹是否存在
 if not exist build ( md build )
 @echo on
 pushd build
-cmake -DCMAKE_INSTALL_PREFIX=C:/MyExe ..
+cmake -DCMAKE_INSTALL_PREFIX=%batdir%runtime ..
 popd
 pause
